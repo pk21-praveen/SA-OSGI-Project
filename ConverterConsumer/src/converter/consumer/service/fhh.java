35e675a2959696df6555b08e25c08e252cb62cfc
pk21-praveen <pk21.praveen@gmail.com>
2019-03-18 09:06:50 +0530

    temp

---
+++ ConverterConsumer/src/converter/consumer/service/fhh.java
@@ -0,0 +1,13 @@
+package converter.consumer.service;
+
+/**
+ * @author PraveenKumar
+ *
+ */
+public class fhh {
+	public static void main(String[] args) {
+		var s = 5;
+		
+		System.out.println(s);
+	}
+}
